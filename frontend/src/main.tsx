a12a497e53b2fe5dcb8c64902a5018c1ad6b1bc6
Jen Person <jen@squid.cloud>
2024-06-24 17:16:51 -0500

    Configure main.tsx to work for local or deployed environments

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,32 @@
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+import App from './App';
 import { SquidContextProvider } from '@squidcloud/react';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <SquidContextProvider
-    options={{
-      appId: import.meta.env.VITE_SQUID_APP_ID,
-      region: import.meta.env.VITE_SQUID_REGION,
-      environmentId: import.meta.env.VITE_SQUID_ENVIRONMENT_ID,
-      squidDeveloperId: import.meta.env.VITE_SQUID_DEVELOPER_ID,
-    }}
-  >
+interface SquidOptions {
+  appId: string;
+  region: 'us-east-1.aws' | 'ap-south-1.aws'; 
+  environmentId: 'dev' | 'prod' | undefined;
+  squidDeveloperId?: string;
+}
+
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error('Failed to find the root element');
+
+const squidOptions: SquidOptions = {
+  appId: import.meta.env.VITE_SQUID_APP_ID,
+  region: import.meta.env.VITE_SQUID_REGION,
+  environmentId: import.meta.env.VITE_SQUID_ENVIRONMENT_ID,
+};
+const squidDeveloperId = import.meta.env.VITE_SQUID_DEVELOPER_ID;
+
+// Only add squidDeveloperId during local development
+if (squidDeveloperId) {
+  squidOptions.squidDeveloperId = squidDeveloperId;
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <SquidContextProvider options={squidOptions}>
     <App />
   </SquidContextProvider>,
 );
